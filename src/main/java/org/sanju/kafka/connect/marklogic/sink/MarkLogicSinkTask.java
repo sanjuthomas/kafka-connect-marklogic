7468a852fc8ee9f7ef0e7c0a7a7590f81c8ac92d
sanjuthomas <sanju@sanju.org>
2017-04-25 23:05:48 -0400

    adding extra logging

--- src/main/java/org/sanju/kafka/connect/marklogic/sink/MarkLogicSinkTask.java
+++ src/main/java/org/sanju/kafka/connect/marklogic/sink/MarkLogicSinkTask.java
@@ -61,8 +61,9 @@ public class MarkLogicSinkTask extends SinkTask {
     
     private void initWriter(final Map<String, String> config){
         
+        logger.info("initWriter called!");
         String writerClazz = config.get(MarkLogicSinkConfig.WRITER_IMPL);
-        logger.info("MarkLogic writer class {}", config);
+        logger.info("MarkLogic writer class {}", writerClazz);
         if(null == writerClazz || writerClazz.trim().isEmpty()){
             this.writer = new MarkLogicDefaultWriter(config);
         }else{
