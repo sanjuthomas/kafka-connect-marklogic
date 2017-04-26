05ee76f5da46718b9b10e2ade2437d3212b4bd41
sanjuthomas <sanju@sanju.org>
2017-04-25 23:16:32 -0400

    adding extra logging

--- src/main/java/org/sanju/kafka/connect/marklogic/MarkLogicAsyncWriter.java
+++ src/main/java/org/sanju/kafka/connect/marklogic/MarkLogicAsyncWriter.java
@@ -34,7 +34,7 @@ public class MarkLogicAsyncWriter extends MarkLogicWriter {
         final ExecutorService fjPool = new ForkJoinPool(8);
         final List<Callable<HttpResponse>> rps = new ArrayList<>();
         try {
-            rs.forEach(r -> {
+            rs.parallelStream().forEach(r -> {
                 rps.add(new RequestProcessor(r));
             });
             final List<Future<HttpResponse>> results = fjPool.invokeAll(rps);
